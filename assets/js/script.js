eeda979172949c50d57075960c5f85b0ad06c5a1
@shubham860 <shubham.chauhan860@gmail.com>
2019-04-20 21:52:51 +0530

    update

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+// border for trending row
+$('.myimg').click(function(){
+ $(this).toggleClass("border");
+ $(this).prev().toggleClass("hidden");
+});
+
+// border for all groups row
+$('.myimgs').click(function(){
+  $(this).toggleClass("border");
+  $(this).parent().siblings(".icon").children().toggleClass("hidden");
+});
+
+
+//Script courtesy
+var message="NoRightClicking";
+function defeatIE() {if (document.all) {(message);return false;}}
+function defeatNS(e) {if
+(document.layers||(document.getElementById&&!document.all)) {
+if (e.which==2||e.which==3) {(message);return false;}}}
+if (document.layers)
+{document.captureEvents(Event.MOUSEDOWN);document.onmousedown=defeatNS;}
+else{document.onmouseup=defeatNS;document.oncontextmenu=defeatIE;}
+document.oncontextmenu=new Function("return false")
